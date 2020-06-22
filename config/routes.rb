04e37aa2df146d19e91bb559984f5441a4741227
Lucas Bide <lucas.bidemachado@gmail.com>
2020-06-22 17:18:27 -0400

    Remove route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
   
   resources :users, only: [:show, :index] do
     resources :friend_requests, only: [:index, :create, :update, :destroy]
